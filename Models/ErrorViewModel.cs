e86785000d23c5773c54619f89611e0e7bd6dde2
mhabi <mhabi@DESKTOP-I338BB7>
2023-03-26 22:42:24 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
